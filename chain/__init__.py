21521441e7df6be47803293703d627312836a540
Weiliang Li <to.be.impressive@gmail.com>
2018-05-01 10:26:59 +0900

    [Add] serialization

--- chain/__init__.py
+++ chain/__init__.py
@@ -1,4 +1,4 @@
-from typing import List
+from functools import total_ordering
 from hashlib import blake2s
 import time
 
@@ -15,13 +15,26 @@ class Block:
         self.target = target
         self.hash = hash
 
-        assert self.is_valid_hash() and self.is_valid_difficulty()
+        assert self.valid
 
     def __repr__(self):
         return 'Block({}, {}, {}, {}, {}, {}, {})'.format(
             repr(self.index), repr(self.prev_hash), repr(self.timestamp),
             repr(self.data), repr(self.nonce), repr(self.target), repr(self.hash)
         )
+
+    def serialize(self) -> dict:
+        return dict(index=self.index,
+                    prev_hash=self.prev_hash,
+                    timestamp=self.timestamp,
+                    data=self.data,
+                    nonce=self.nonce,
+                    target=self.target,
+                    hash=self.hash)
+
+    @property
+    def valid(self) -> bool:
+        return self.is_valid_hash() and self.is_valid_difficulty()
 
     def is_valid_hash(self) -> bool:
         return self.recalculate_hash() == self.hash
@@ -44,25 +57,44 @@ class Block:
         return int(hash, 16) <= int(target, 16)
 
 
+@total_ordering
 class BlockChain:
 
-    def __init__(self):
-        self.blocks = [BlockChain.genesis()]
-        self.interval = 5  # 5s per block
+    def __init__(self, blocks=[]):
+        self.blocks = [BlockChain.genesis()] if not blocks else blocks
+        self._interval = 5  # 5s per block
 
-    @staticmethod
-    def genesis() -> Block:
-        args = (0, '0', int(time.time()), 'Genesis Block')
-        nonce = 0
-        # suppose this target's difficulty = 1
-        target = '0000ffff00000000000000000000000000000000000000000000000000000000'
-        while True:
-            hash = Block.calculate_hash(*args, nonce=nonce, target=target)
-            if Block.validate_difficulty(hash, target):
-                break
-            else:
-                nonce += 1
-        return Block(*args, nonce=nonce, target=target, hash=hash)
+    def __len__(self):
+        return self.length
+
+    def __eq__(self, other):
+        if self.length != other.length:
+            return False
+        return all([b1.hash == b2.hash for b1, b2 in zip(self.blocks, other.blocks)])
+
+    def __lt__(self, other):
+        return self.length < other.length
+
+    @property
+    def interval(self) -> int:
+        return self._interval
+
+    @property
+    def latest_block(self) -> Block:
+        return self.blocks[-1]
+
+    @property
+    def length(self) -> int:
+        return len(self.blocks)
+
+    def serialize(self) -> dict:
+        return dict(
+            blocks=[b.serialize() for b in self.blocks]
+        )
+
+    def replace(self, other) -> None:
+        if self < other and other.is_valid_chain():
+            self.blocks = other.blocks
 
     def retarget(self) -> str:
         lb = self.latest_block
@@ -80,18 +112,18 @@ class BlockChain:
             new_target = int(lb.target, 16) * actual_timespan / target_timespan
             return f'{int(new_target):x}'.rjust(64, '0')
 
-    @property
-    def latest_block(self) -> Block:
-        return self.blocks[-1]
+    def validate_blocks(self, left: int, right: int):
+        assert 0 <= left < right < self.length
+        mini_blocks = self.blocks[left:right + 1]
+        are_all_valid = all([b.valid for b in mini_blocks])
+        are_all_adjacent = all([
+            BlockChain.are_blocks_adjacent(cur_block, prev_block)
+            for prev_block, cur_block in zip(mini_blocks[:-1], mini_blocks[1:])
+        ])
+        return are_all_valid and are_all_adjacent
 
-    @staticmethod
-    def are_blocks_adjacent(block: Block, prev_block: Block) -> bool:
-        is_valid_block = block.is_valid_hash() and block.is_valid_difficulty()
-        is_valid_next = block.index == prev_block.index + 1 and block.prev_hash == prev_block.hash
-        return is_valid_block and is_valid_next
-
-    def is_block_next(self, block: Block) -> bool:
-        return BlockChain.are_blocks_adjacent(block, self.latest_block)
+    def is_valid_chain(self):
+        return self.validate_blocks(0, self.length - 1)
 
     def generate_next(self, data: str) -> Block:
         lb = self.latest_block
@@ -106,22 +138,55 @@ class BlockChain:
                 nonce += 1
         return Block(*args, nonce=nonce, target=target, hash=hash)
 
+    def is_next_block(self, block: Block) -> bool:
+        return BlockChain.are_blocks_adjacent(block, self.latest_block)
+
     def mine(self, data: str) -> None:
         b = self.generate_next(data)
-        if self.is_block_next(b):
+        if self.is_next_block(b):
             self.blocks.append(b)
         else:
             raise ValueError('Invalid next block')
+
+    @staticmethod
+    def are_blocks_adjacent(block: Block, prev_block: Block) -> bool:
+        is_valid_block = block.valid
+        is_valid_next = block.index == prev_block.index + 1 and block.prev_hash == prev_block.hash
+        return is_valid_block and is_valid_next
+
+    @staticmethod
+    def genesis() -> Block:
+        args = (0, '0', int(time.time()), 'Genesis Block')
+        nonce = 0
+        # suppose this target's difficulty = 1
+        target = '0000ffff00000000000000000000000000000000000000000000000000000000'
+        while True:
+            hash = Block.calculate_hash(*args, nonce=nonce, target=target)
+            if Block.validate_difficulty(hash, target):
+                break
+            else:
+                nonce += 1
+        return Block(*args, nonce=nonce, target=target, hash=hash)
+
+    @staticmethod
+    def deserialze(other: dict):
+        blocks = [Block(**d) for d in other['blocks']]
+        return BlockChain(blocks=blocks)
 
 
 if __name__ == '__main__':
     last = time.time()
     b = BlockChain()
     print(b.latest_block)
-    for i in range(200):
+    for i in range(100):
         b.mine('a')
         interval = time.time() - last
         last = time.time()
         print(interval)
+
+    print(b.validate_blocks(0, 1))
+    print(b.validate_blocks(1, 3))
+    print(b.is_valid_chain())
+    print(BlockChain.deserialze(b.serialize()) == b)
 
     print(b.blocks)
--- chain/test.py
+++ chain/test.py
@@ -1,10 +1,30 @@
 import unittest
-from . import Block
+import time
+
+from . import Block, BlockChain
 
 
 class TestBlockChain(unittest.TestCase):
     def test_block(self):
-        pass
+        args = (0, '0', int(time.time()), 'test')
+        nonce = 0
+        target = 'f' * 64
+        hash = Block.calculate_hash(*args, nonce, target)
+        b = Block(*args, nonce=nonce, target=target, hash=hash)
+        self.assertTrue(b.valid)
+        b.hash = 'aaa'
+        self.assertFalse(b.valid)
+
+    def test_blockchain(self):
+        bc = BlockChain()
+        for i in range(10):
+            bc.mine('a')
+
+        self.assertTrue(bc.validate_blocks(0, 1))
+        self.assertTrue(bc.validate_blocks(1, 3))
+        self.assertTrue(bc.is_valid_chain())
+        self.assertTrue(BlockChain.deserialze(bc.serialize()).is_valid_chain())
+        self.assertTrue(BlockChain.deserialze(bc.serialize()) == bc)
 
 
 if __name__ == '__main__':
